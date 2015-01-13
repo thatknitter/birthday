56f1fe4c26829e925b6b22dad28ee3d89d891734
thatknitter <kblewis.flt@gmail.com>
2015-01-13 11:42:20 -0600

    finished

---
+++ birthday/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What is your birthday? Enter Month/Day");
+            string birthday = System.Console.ReadLine();
+
+            System.Console.WriteLine("Press any key to close.");
+            System.Console.ReadKey();
+        }
+    }
+}
